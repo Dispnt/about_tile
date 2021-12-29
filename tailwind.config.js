b372ee7b8ab0ce9fef3e3328e33f4576ea8a6b55
dispnt <yuyuanjiajian@gmail.com>
2021-12-29 22:49:36 +0800

    some tailwind stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     borderRadius: {
       'none': '0',
      DEFAULT: '0.25rem',
+     'md':'23px',
      'lg': '32px',
      'full': '9999px',
     },
@@ -13,11 +14,23 @@ module.exports = {
         'background':
         {
           DEFAULT:'#f7f2f2',
-          dark:'#090C10'
+          dark:'#090C10',
+          menu:'#0000000a',
+          darkmode:'#f0f2f8',
         } ,
+        'menu':'#0d1117',
+        'spotify':'#6ed2b7',
+        'darkmode':'#0d1117',
         'twitter':'#98d0ff',
-        'steam':'#353230'
+        'steam':'#353230',
        },
+       width: {
+        '0.25':'3px',
+        '23': '5.625rem',
+      },
+      height:{
+        '0.25':'3px',
+      },
        minHeight:{
          'lg': '280px',
          'sm': '164px'
